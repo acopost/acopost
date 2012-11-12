2ab1fdd938cf398a54281c615990c608bc7a29ad
Ulrik Sandborg-Petersen <ulrikp@emergence.dk>
2012-11-13 00:13:44 +0100

    Moved to an Autoconf / Automake toolset.

---
+++ rmfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+rm -f `find . -type f -a -name libtool`
+rm -f `find . -type f -a -name gmon.out`
+rm -f configure
+rm -f ltmain.sh
+rm -f libtool
+rm -f install-sh
+rm -f aclocal.m4
+rm -f config.guess
+rm -f config.sub
+rm -f config.log
+rm -f configure.scan
+rm -f depcomp
+rm -f missing
+rm -f mkinstalldirs
+rm -f compile
+rm -f `find . -name Makefile.in`
+rm -f `find . -name Makefile`
+rm -rf autom4te.cache/
+rm -f *~
