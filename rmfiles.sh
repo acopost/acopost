9170d1a0c26bfd09ce7e66bd925393259e31d721
Giulio Paci <giuliopaci@gmail.com>
2013-05-30 10:28:30 +0200

    Changed rmfiles.sh to use "make distclean".

--- rmfiles.sh
+++ rmfiles.sh
@@ -1,28 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+
+test -f Makefile && make distclean
+rm -f aclocal.m4
+rm -rf autom4te.cache/
+rm -f `find . -name Makefile.in`
+rm -f configure
+rm -f depcomp
+rm -f install-sh
+rm -f missing
+
 rm -f `find . -type f -a -name libtool`
 rm -f `find . -type f -a -name gmon.out`
 rm -f `find doc -type f -a -name *.log`
-rm -f configure
 rm -f ltmain.sh
 rm -f libtool
-rm -f install-sh
-rm -f aclocal.m4
 rm -f config.guess
 rm -f config.sub
-rm -f config.log
-rm -f config.h
-rm -f config.status
-rm -f stamp-h* 
-rm -f configure.scan
-rm -f depcomp
-rm -f missing
-rm -f mkinstalldirs
 rm -f compile
-rm -f `find . -name Makefile.in`
-rm -f `find . -name Makefile`
-rm -rf autom4te.cache/
-rm -f acopost.spec
-rm -f *~
-rm -rf src/.deps
-rm -rf tests/*.test
+rm -f mkinstalldirs
+
+rm -f configure.scan
 rm -f valgrind.out.txt out.txt
