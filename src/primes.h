066b055eb039c1531087826bc32bead0c3a71552
Giulio Paci <giuliopaci@gmail.com>
2013-08-04 01:44:26 +0200

    Updated primes.h.

--- src/primes.h
+++ src/primes.h
@@ -1,38 +1,85 @@
 /*
   Prime numbers
 
-  Copyright (C) 1997-2001 The DAWAI Team
-            (C) 2010 Tiago Tresoldi
+  Copyright (c) 2013, ACOPOST Developers Team
+  All rights reserved.
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+   * Neither the name of the ACOPOST Developers Team nor the names of
+     its contributors may be used to endorse or promote products
+     derived from this software without specific prior written
+     permission.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
 */
 
 #ifndef PRIMES_H
 #define PRIMES_H
 
-/* ------------------------------------------------------------ */
-typedef unsigned long ulong;
+#include <stdlib.h>
 
-/* ------------------------------------------------------------ */
-/* rabin's probablistic primetest-algorithm
- * returns true if number is a prime by testing it a few times
+/**
+ * Evaluate (x+y)%m.
+ *
+ * The result is guaranteed to be correct for every possible input
+ * values.
  */
-int primes_rabin (unsigned long no, unsigned long times);
+size_t modular_addition_size_t(size_t x, size_t y, size_t m);
 
-/* returns the next prime after a given number */
-unsigned long primes_next (unsigned long no, unsigned long times);
+/**
+ * Evaluate (x*y)%m.
+ *
+ * The result is guaranteed to be correct for every possible input
+ * values.
+ */
+size_t modular_product_size_t(size_t x, size_t y, size_t m);
 
-/* ------------------------------------------------------------ */
+/**
+ * Evaluate (x^y)%m.
+ *
+ * The result is guaranteed to be correct for every possible input
+ * values.
+ */
+size_t modular_power_size_t(size_t x, size_t y, size_t m);
+
+/**
+ * Perform deterministic variant of miller-rabin test.
+ *
+ * The result is guaranteed to be correct for every possible input
+ * values up to 2^64.
+ *
+ * @return 1 if x is prime, 0 if x is composite
+ */
+int miller_rabin_size_t(size_t x);
+
+/**
+ * Given an input number, output the first prime number greater than
+ * or equal to the input.
+ *
+ * @return the first prime number greater than or equal to x, or 0 if
+ * no such number exist within the size_t range (i.e., if an overflow
+ * happens).
+ */
+size_t miller_rabin_next_prime_size_t(size_t x);
+
 #endif
