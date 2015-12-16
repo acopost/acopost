7a616b0475813ae239202d29c5b82196b728862a
Giulio Paci <giuliopaci@gmail.com>
2015-12-16 04:52:07 +0100

    Fixed encoding for gis.h.

--- src/gis.h
+++ src/gis.h
@@ -1,7 +1,7 @@
 /*
   Generalized Iterative Scaling
 
-  Copyright (c) 2001-2002, Ingo SchrÃ¶der
+  Copyright (c) 2001-2002, Ingo Schröder
   Copyright (c) 2007-2013, ACOPOST Developers Team
   All rights reserved.
 
